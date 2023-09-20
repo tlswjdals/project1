972e461363dd5b4f22265992f54103b2d430bde2
tlswjdals0000@gmail.com <ruddlf5073!!>
2023-09-20 15:18:25 +0900

    -랑 *까지함

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 ##함수(=메서드) 선언부
 def add_func(n1,n2):
     return n1 + n2
-
+def add_func(n1,n2):
+    return n1 - n2
+def add_func(n1,n2):
+    return n1 * n2
 
 ## 전역 변수부
 num1= 100
@@ -9,3 +12,8 @@ num2 = 200
 ## 메인 코드부
 res = num1 + num2
 print(num1, '+', num2, '=', res)
+res = num1 - num2
+print(num1, '-', num2, '=', res)
+
+res = num1 * num2
+print(num1, '*', num2, '=', res)
